3c8aae1a729d9f3ee0944c70fc7b8517a80f7f9a
Omel <michelson45@gmail.com>
2024-03-31 18:09:34 +0200

    Initial commit

---
+++ src/main/java/dev/omel/name/main/Main.java
@@ -0,0 +1,23 @@
+package dev.omel.name.main;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class Main extends Plugin {
+
+    @Override
+    public void onEnable(){
+        registerCommands();
+        registerListener();
+
+    }
+
+    public void onDisable(){}
+
+    private void registerCommands(){
+    }
+
+    private void registerListener() {
+
+    }
+
+}
